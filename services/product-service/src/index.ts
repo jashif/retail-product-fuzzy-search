df15d6a713beb674b7cd6c74e0df739e90c3a304
Jashif <jashif.aboobacker2@ingka.com>
2024-10-26 22:13:17 +0200

    chore: add open api validator

--- services/product-service/src/index.ts
+++ services/product-service/src/index.ts
@@ -1,11 +1,16 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { Application, Request, Response } from "express";
+import * as OpenApiValidator from "express-openapi-validator";
+import * as path from "path";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 import { errorMiddleware } from "./api/middlewares/error-middleware";
 import { requestMiddleware } from "./api/middlewares/request-logger";
 import { getRoutes } from "./api/routes";
 import { MemoryStore } from "./core/db/mem-store";
 import { generateRandomProducts } from "./utils/product-mock";
+
 dotenv.config();
 
 async function startServer() {
@@ -18,6 +23,18 @@ async function startServer() {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(requestMiddleware);
+
+  const specPath = path.join(__dirname, "./api/spec.yaml");
+
+  const apiSpec = YAML.load(specPath);
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiSpec));
+  app.use(
+    OpenApiValidator.middleware({
+      apiSpec: specPath,
+      validateRequests: true,
+      validateResponses: false,
+    })
+  );
   // Application routing
   app.get("/", (req: Request, res: Response) => {
     res.status(200).send({ data: "HI there" });
